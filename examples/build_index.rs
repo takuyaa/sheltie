3ed44edee8477ff049bd0b1c1dedb042c716235d
Takuya Asano <takuya.a@gmail.com>
2020-03-23 21:54:50 +0900

    Implement JSON loader

---
+++ examples/build_index.rs
@@ -0,0 +1,31 @@
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+use std::io::BufRead;
+use Result;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Record {
+    id: String,
+    text: String,
+}
+
+fn parse_json(data: &str) -> serde_json::Result<Record> {
+    let record: Record = serde_json::from_str(data)?;
+    Ok(record)
+}
+
+fn main() -> Result<(), String> {
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.map_err(|err| format!("Failed to get line from stdin: {}", err))?;
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let record = parse_json(&line).map_err(|err| format!("Failed to parse JSON: {}", err))?;
+        println!("{}: {}", record.id, record.text);
+    }
+    Ok(())
+}
