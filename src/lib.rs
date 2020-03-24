56c7a3c8dd5066b677883ba745d7fa03fbdc802e
Takuya Asano <takuya.a@gmail.com>
2020-03-24 15:45:21 +0900

    Implement first inverted index

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq)]
 pub struct Token {
     term: String,
@@ -14,10 +17,61 @@ pub fn analyze(text: String) -> Vec<Token> {
         .collect::<Vec<Token>>()
 }
 
+#[derive(Debug)]
+pub struct Index {
+    inverted_index: RefCell<HashMap<String, Vec<u64>>>,
+    max_doc_id: u64,
+}
+
+impl Index {
+    pub fn new() -> Index {
+        Index {
+            inverted_index: RefCell::new(HashMap::new()),
+            max_doc_id: 0u64,
+        }
+    }
+
+    pub fn add(&mut self, text: String) {
+        let doc_id = self.max_doc_id + 1;
+        let tokens = analyze(text);
+        for token in tokens {
+            let map = self.inverted_index.get_mut();
+            if let Some(postings_list) = map.get_mut(&token.term) {
+                postings_list.push(doc_id);
+            } else {
+                let posting_list = vec![doc_id];
+                map.insert(token.term, posting_list);
+            }
+        }
+        self.max_doc_id = doc_id;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::analyze;
+    use super::Index;
     use super::Token;
+
+    #[test]
+    fn test_index() {
+        let mut index = Index::new();
+        assert_eq!(index.max_doc_id, 0);
+        index.add(String::from("lorem ipsum"));
+        assert_eq!(index.max_doc_id, 1);
+        let posting_list_of_test = index
+            .inverted_index
+            .get_mut()
+            .get(&"lorem".to_string())
+            .unwrap();
+        assert_eq!(posting_list_of_test.len(), 1);
+        let posting_list_of_test = index
+            .inverted_index
+            .get_mut()
+            .get(&"ipsum".to_string())
+            .unwrap();
+        assert_eq!(posting_list_of_test.len(), 1);
+    }
 
     #[test]
     fn test_analyze() {
