4c6ecda4eaed9721cdde3a609011aa7a312ecabd
Takuya Asano <takuya.a@gmail.com>
2020-03-26 15:25:10 +0900

    Change PostingsList structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Token {
     token: String,
 }
@@ -19,12 +19,25 @@ pub fn analyze(text: &String) -> Vec<Token> {
 
 #[derive(Debug)]
 pub struct PostingsList {
-    data: Vec<usize>,
+    docs: Vec<usize>,
+    freqs: Vec<u32>,
 }
 
 impl PostingsList {
+    pub fn new() -> PostingsList {
+        PostingsList {
+            docs: vec![],
+            freqs: vec![],
+        }
+    }
+
     pub fn len(&self) -> usize {
-        self.data.len()
+        self.docs.len()
+    }
+
+    pub fn add(&mut self, doc_id: usize, freq: u32) {
+        self.docs.push(doc_id);
+        self.freqs.push(freq);
     }
 }
 
@@ -43,15 +56,25 @@ impl Index {
     }
 
     pub fn add(&mut self, text: &String) {
-        let doc_id = self.max_doc_id + 1;
         let tokens = analyze(&text);
-        for token in tokens {
-            let map = self.inverted_index.get_mut();
-            if let Some(postings_list) = map.get_mut(&token.token) {
-                postings_list.data.push(doc_id);
+        let freq_map = {
+            let mut freq_map = HashMap::<String, u32>::new();
+            for token in tokens {
+                let freq = freq_map.entry(token.token).or_insert(0);
+                *freq += 1;
+            }
+            freq_map
+        };
+
+        let doc_id = self.max_doc_id + 1;
+        let inverted_index = self.inverted_index.get_mut();
+        for (token, freq) in &freq_map {
+            if let Some(postings_list) = inverted_index.get_mut(token) {
+                postings_list.add(doc_id, *freq);
             } else {
-                let posting_list = PostingsList { data: vec![doc_id] };
-                map.insert(token.token, posting_list);
+                let mut posting_list = PostingsList::new();
+                posting_list.add(doc_id, *freq);
+                inverted_index.insert(token.clone(), posting_list);
             }
         }
         self.max_doc_id = doc_id;
@@ -66,22 +89,20 @@ mod tests {
 
     #[test]
     fn test_index() {
-        let mut index = Index::new();
-        assert_eq!(index.max_doc_id, 0);
-        index.add(&String::from("lorem ipsum"));
+        let index = {
+            let mut index = Index::new();
+            assert_eq!(index.max_doc_id, 0);
+            index.add(&String::from("two one two"));
+            index
+        };
         assert_eq!(index.max_doc_id, 1);
-        let posting_list_of_test = index
-            .inverted_index
-            .get_mut()
-            .get(&"lorem".to_string())
-            .unwrap();
-        assert_eq!(posting_list_of_test.len(), 1);
-        let posting_list_of_test = index
-            .inverted_index
-            .get_mut()
-            .get(&"ipsum".to_string())
-            .unwrap();
-        assert_eq!(posting_list_of_test.len(), 1);
+        let inverted_index = index.inverted_index.borrow();
+
+        let posting_list_one = inverted_index.get(&"one".to_string()).unwrap();
+        assert_eq!(posting_list_one.len(), 1);
+
+        let posting_list_of_two = inverted_index.get(&"two".to_string()).unwrap();
+        assert_eq!(posting_list_of_two.len(), 1);
     }
 
     #[test]
